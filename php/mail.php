a2c5150c80f425c10f165c517b0b5b67e8099c93
marzia1188 <parigi1188>
2014-05-24 00:10:45 +0200

    modifiche varie

--- php/mail.php
+++ php/mail.php
@@ -3,29 +3,29 @@
     include_once 'configuration.php'
 
     /*Istanzio la classe PHPMailer*/
-    $messaggio = new PHPMailer();
+    $messaggio_invio = new PHPMailer();
 
     /*Definisco le intestazioni e il corpo del messaggio*/
     $nome = htmlspecialchars(trim ($_POST['nome']));
     $email = htmlspecialchars(trim ($_POST['email']));
     $messaggio = htmlspecialchars(trim ($_POST['messaggio']));
-    $messaggio->From = $email;
+    $messaggio_invio->From = $email;
 
-    $messaggio->AddAddress($email_sito);
-    $messaggio->AddReplyTo($email);
-    $messaggio->Subject = $oggetto_stampe + " "+ $nome;
-    $messaggio->Body=stripslashes($messaggio);
+    $messaggio_invio->AddAddress($email_sito);
+    $messaggio_invio->AddReplyTo($email);
+    $messaggio_invio->Subject = $oggetto_stampe + " "+ $nome;
+    $messaggio_invio->Body=stripslashes($messaggio);
 
     //definiamo i comportamenti in caso di invio corretto
     //o di errore
-    if(!$messaggio->Send()){
-      echo $messaggio->ErrorInfo;
+    if(!$messaggio_invio->Send()){
+      echo $messaggio_invio->ErrorInfo;
     }else{
       echo 'Email inviata correttamente!';
     }
 
     //chiudiamo la connessione
-    unset($messaggio);
+    unset($messaggio_invio);
 
 
 ?>
