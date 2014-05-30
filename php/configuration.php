0c0e6b1216e6d73c06eed4e3fa843663b933ee41
marzia1188 <parigi1188>
2014-05-30 20:34:26 +0200

    modifiche varie

--- php/configuration.php
+++ php/configuration.php
@@ -1,6 +1,6 @@
 <?php
 
-    $email_sito = info@universitaliasrl.it;
-    $oggetto_info = "Richiesta informazioni/commenti dal sito di Universitalia"
+    $email_sito = "marzia.magheri@gmail.com";
+    $oggetto_info = "Richiesta informazioni/commenti dal sito di Universitalia";
     $oggetto_stampe = "Invio file dal sito Universitalia per stampa";
 ?>
--- php/mail.php
+++ php/mail.php
@@ -1,27 +1,33 @@
 <?php
     require "PHPMailer-master/class.phpmailer.php";
-    include_once 'configuration.php'
+    include_once 'configuration.php';
 
     /*Istanzio la classe PHPMailer*/
     $messaggio_invio = new PHPMailer();
 
     /*Definisco le intestazioni e il corpo del messaggio*/
-    $nome = htmlspecialchars(trim ($_POST['nome']));
+    $nome = htmlspecialchars(trim ($_POST['name']));
     $email = htmlspecialchars(trim ($_POST['email']));
-    $messaggio = htmlspecialchars(trim ($_POST['messaggio']));
+    $messaggio = htmlspecialchars(trim ($_POST['message']));
     $messaggio_invio->From = $email;
 
     $messaggio_invio->AddAddress($email_sito);
     $messaggio_invio->AddReplyTo($email);
-    $messaggio_invio->Subject = $oggetto_stampe + " "+ $nome;
+    $messaggio_invio->Subject = $oggetto_stampe . ' ' . $nome;
     $messaggio_invio->Body=stripslashes($messaggio);
 
     //definiamo i comportamenti in caso di invio corretto
     //o di errore
     if(!$messaggio_invio->Send()){
-      echo $messaggio_invio->ErrorInfo;
+      $stringa = $messaggio_invio->ErrorInfo;
     }else{
-      echo 'Email inviata correttamente!';
+        //echo 'Email inviata correttamente!';
+        header('Location: ../index.html#domande_commenti');
+        $stringa= "Email inviata correttamente!";
+        echo "<script language=\"JavaScript\">\n";
+        echo "alert(\"$stringa\");\n";
+        echo "</script>";
+        //exit();
     }
 
     //chiudiamo la connessione
