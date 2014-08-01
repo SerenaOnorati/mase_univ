c96b5e61af606e30e68eaf82ce0c0cd85a4cf617
Serena Onorati <serenaonorati@hotmail.it>
2014-08-01 14:48:47 +0200

    configuration

--- configuration.php
+++ configuration.php
@@ -9,4 +9,26 @@
     $formato_mapo = array('35 x 50','35 x 70','50 x 70','70 x 100','AO 84,1 x 118', 'A1 59,9 x 84,1', 'A2 42 x 59,4');
     $formato_std =array('B/N A4','B/N A3','Colori A4','Colori A3');
 
+    //classe connessione db
+    class MysqlClass
+    {
+        // parametri per la connessione al database
+        private $nomehost = "sql41.securesites.it";
+        private $nomeuser = "a_2632";
+        private $password = "Etei2wex";
+
+        // controllo sulle connessioni attive
+        private $attiva = false;
+
+        // funzione per la connessione a MySQL
+        public function connetti()
+        {
+            if(!$this->attiva)
+            {
+                $connessione = mysql_connect($this->nomehost,$this->nomeuser,$this->password);
+            }else{
+                return true;
+            }
+        }
+    }
 ?>
