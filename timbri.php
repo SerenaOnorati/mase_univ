f877f50af6ef5e7bf33a197f3509e5b7dc119034
marzia1188 <parigi1188>
2014-08-20 17:03:01 +0200

    timbri aggiustamenti vari

--- timbri.php
+++ timbri.php
@@ -54,26 +54,14 @@
 												<h2> Timbri in  <strong>24 ore</strong></h2>
 											</header>
 											<span class="image image-full"><img src="images/timbri_testata.jpg" alt=""/></span>
-                                            <span><div class="row"  style="padding-left: 100px">
-
-                                               <!-- <div class="row half">
-                                                    <div class="6u"><span class="image"><img src="images/PR_4911_P4_H_ecoblack.jpg"><img src="images/IM_MCI_4911_Handwerker_NL.jpg" alt="cod. 4911 - Max. dimensioni piastra  di testo 38 x 14 mm"/>
-                                                           </span></div>
-                                                    <div class="6u"><span class="image"><img src="images/PR_4912_P4_HR_ecoblack.jpg"><img src="images/IM_MCI_4912_RotKreuzHaus.jpg"><p style="text-align: center"><strong>cod. 4912</strong> -<br>Max. dimensioni piastra
-                                                            di testo 47 x 18 mm</p></span></div>
-                                                </div>
-                                                <div class="row half">
-                                                    <div class="6u"><span class="image"><img src="images/PR_4913_P4_H_ecoblack.jpg"><img src="images/IM_MCI_4913_Pesca_4c_IT.jpg"><p style="text-align: center"><strong>cod. 4913</strong> <br> Max. dimensioni piastra<br>
-                                                            di testo 58 x 22 mm</p></span></div>
-                                                    <div class="6u"><span class="image"><img src="images/PR_4914_P4_ecoblack.jpg"><img src="images/IM_MCI_4914_Cortica_4c_PT.jpg"><p style="text-align: center"><strong>cod. 4914</strong> <br> Max. dimensioni piastra <br>
-                                                            di testo 64 x 26 mm</p></span></div>
-                                                </div>-->
-                                                <div class="row half">
-                                                   <div class="6u"><span class="image"><img src="images/4911.gif"><p style="text-align: center"><strong>cod. 4911</strong> <br> Max. dimensioni piastra<br>
-                                                                di testo 38 x 14 mm</p></span></div>
-                                                    <div class="6u"><span class="image"><img src="images/4912.gif"><p style="text-align: center"><strong>cod. 4912</strong> <br> Max. dimensioni piastra<br>
-                                                                di testo 47 x 18 mm</p></span></div>
-                                                </div>
+                                            <span>
+                                                <div class="row"  style="padding-left: 100px">
+                                                    <div class="row half">
+                                                       <div class="6u"><span class="image"><img src="images/4911.gif"><p style="text-align: center"><strong>cod. 4911</strong> <br> Max. dimensioni piastra<br>
+                                                                    di testo 38 x 14 mm</p></span></div>
+                                                        <div class="6u"><span class="image"><img src="images/4912.gif"><p style="text-align: center"><strong>cod. 4912</strong> <br> Max. dimensioni piastra<br>
+                                                                    di testo 47 x 18 mm</p></span></div>
+                                                    </div>
                                                     <div class="row half">
                                                         <div class="6u"><span class="image"><img src="images/4913.gif"><p style="text-align: center"><strong>cod. 4913</strong> <br> Max. dimensioni piastra<br>
                                                                     di testo 58 x 22 mm</p></span></div>
@@ -87,8 +75,8 @@
                                                                     di testo 75 x 38 mm</p></span></div>
                                                     </div>
 
-                                            </div>
-                                                </span>
+                                                </div>
+                                            </span>
 										</article>
 								
 								</div>
