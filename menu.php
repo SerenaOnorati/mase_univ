41d518e83a128049264955ab7244e8e1d59b0897
marzia1188 <parigi1188>
2014-08-20 17:18:12 +0200

    modifica collegamenti ipertestuali

--- menu.php
+++ menu.php
@@ -1,7 +1,7 @@
 <!-- Nav -->
 <nav id="nav">
     <br>
-    <h1 id="logo"><a href="#">Universitalia</a></h1><br><br><br>
+    <h1 id="logo"><a href="index.php">Universitalia</a></h1><br><br><br>
     <ul id="icone">
         <li>
             <a class="fa fa-home" href="index.php"><span>Home</span></a>
@@ -25,7 +25,7 @@
         </li>
 
         <li>
-            <a href="" class="fa fa-print"><span>Stampe Digitali</span></a>
+            <a href="plotter.php" class="fa fa-print"><span>Stampe Digitali</span></a>
             <ul>
                 <li><a href="plotter.php">Plotter</a></li>
                 <li><a href="stampe.php">Stampe</a></li>
@@ -33,13 +33,13 @@
             </ul>
         </li>
         <li>
-            <a href="" class="fa fa-book"><span>Editoria</span></a>
+            <a href="http://universitaliaeditrice.it/" target="_blank" class="fa fa-book"><span>Editoria</span></a>
             <ul>
                 <li><a href="http://universitaliaeditrice.it/" target="_blank">Pubblica con noi</a></li>
             </ul>
         </li>
         <li>
-            <a href="" class="fa fa-shopping-cart"><span>UniShop</span></a>
+            <a href="preventivo.php" class="fa fa-shopping-cart"><span>UniShop</span></a>
             <ul>
                 <li><a href="preventivo.php">Preventivo Stampe</a></li>
             </ul>
