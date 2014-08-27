b1ef493571c9b15198366863db66a2164f068073
marzia1188 <parigi1188>
2014-08-27 18:51:22 +0200

    pre-cancellazione

--- ricerca.html.php
+++ ricerca.html.php
@@ -13,14 +13,14 @@
                         <h3 class="fa fa-user" style="color: #ed786a">Autore</h3>
                     </div>
                     <div class="3u">
-                        <input id="insautore" name="insautore" placeholder="Inserisci Autore" type="text" class="text"">
+                        <input id="insautore" name="insautore" placeholder="Inserisci Autore" type="text" class="text"/>
                     </div>
 
                     <div class="3u">
                         <h3 class="fa fa-barcode" style="color: #ed786a">ISBN</h3>
                     </div>
                     <div class="3u">
-                        <input id="insisbn" name="institolo" placeholder="Inserisci ISBN" type="text" class="text">
+                        <input id="insisbn" name="institolo" placeholder="Inserisci ISBN" type="text" class="text"/>
                     </div>
                 </div>
                 <div class="row">
@@ -28,14 +28,14 @@
                         <h3 class="fa fa-book" style="color: #ed786a">Titolo</h3>
                     </div>
                     <div class="3u">
-                        <input id="institolo" name="insautore" placeholder="Inserisci Titolo" type="text" class="text"">
+                        <input id="institolo" name="insautore" placeholder="Inserisci Titolo" type="text" class="text"/>
                     </div>
 
                     <div class="3u">
                         <h3 class="fa fa-map-marker" style="color: #ed786a">Locazione</h3>
                     </div>
                     <div class="3u">
-                        <input id="inslocazione" name="institolo" placeholder="Inserisci Locazione" type="text" class="text">
+                        <input id="inslocazione" name="institolo" placeholder="Inserisci Locazione" type="text" class="text"/>
                     </div>
                 </div>
                 <div class="row">
@@ -43,23 +43,28 @@
                         <h3 class="fa fa-building" style="color: #ed786a">Casa Editrice</h3>
                     </div>
                     <div class="3u">
-                        <input id="inscasaeditrice" name="insautore" placeholder="Inserisci Casa Editrice" type="text" class="text"">
+                        <input id="inscasaeditrice" name="insautore" placeholder="Inserisci Casa Editrice" type="text" class="text"/>
                     </div>
 
                     <div class="3u">
                         <h3 class="fa fa-calendar" style="color: #ed786a">Anno Acquisto</h3>
                     </div>
                     <div class="3u">
-                        <input id="insannoacquisto" name="insannoacquisto" placeholder="Inserisci Anno" type="text" class="text">
+                        <input id="insannoacquisto" name="insannoacquisto" placeholder="Inserisci Anno" type="text" class="text"/>
                     </div>
                 </div>
-
-
-                <div class="row" style="align-content: center !important">
-                   <div class="12u" >
-                       <button id="ricerca" class="button button-icon fa fa-search">Ricerca</button>
-                   </div>
-                </div>
+                <script type="text/javascript">
+                    function submitform()
+                    {
+                        this.document.forms["ricerca"].submit();
+                    }
+                </script>
+                <br>
+                <ul class="actions" style="align-content: center!important">
+                    <li>
+                        <a href="javascript: submitform()" class="button button-icon fa fa-search">Cerca</a>
+                    </li>
+                </ul>
             </form>
         </article>
     </div>
--- ricerca.php
+++ ricerca.php
@@ -11,108 +11,126 @@
     {
         include 'db.inc.php';
 
-        try
-        {
-            if(isset($_POST['institolo'])){
-                $titolo = "%".trim($_POST['institolo'])."%";
 
-            }
-            else
-            {
-                $titolo = "%%";
 
-            }
-            echo "Titolo: ".$_POST['institolo'];
-            if(isset($_POST['insautore']))
-            {
-                $autore = "%".$_POST['insautore']."%";
-            }
-            else
-            {
-                $autore = "%%";
+        if(isset($_POST['institolo'])){
+            $titolo = "%".trim($_POST['institolo'])."%";
 
-            }
-            echo "<br>Autore: ".$autore;
-            if(isset($_POST['inscasaeditrice']))
-            {
-                $casaeditrice = "%".trim($_POST['inscasaeditrice'])."%";
-
-            }
-            else
-            {
-                $casaeditrice = "%%";
-            }
-            echo "<br>CasaEditrice: ".$casaeditrice;
-
-            if(isset($_POST['inslocazione']))
-            {
-                $locazione = "%".trim($_POST['inslocazione'])."%";
-            }
-            else
-            {
-                $locazione = "%%";
-
-            }
-            echo "<br>Locazione: ".$locazione;
-
-            if(isset($_POST['insisbn']))
-            {
-                $isbn = "%".trim($_POST['insisbn'])."%";
-            }
-            else
-            {
-                $isbn = "%%";
-
-            }
-            echo "<br>ISBN: ".$isbn;
-
-            if(isset($_POST['insannoacquisto']))
-            {
-                $anno_acquisto = "%".trim($_POST['insannoacquisto'])."%";            }
-            else
-            {
-                $anno_acquisto = "%%";
-
-            }
-            echo "<br>annoacquisto: ".$anno_acquisto;
-
-            $sql = 'SELECT * FROM libro INNER JOIN casa_editrice on libro.id_casa_editrice = casa_editrice.id_casa_editrice
-                    WHERE titolo LIKE :titolo AND autore LIKE :autore AND locazione LIKE :locazione
-                    AND nome LIKE :casaeditrice AND isbn LIKE :isbn AND anno_acquisto LIKE :anno_acquisto';
-            $s = $pdo->prepare($sql);
-            $s->bindValue(':titolo', $titolo, PDO::PARAM_STR);
-            $s->bindValue(':autore', $autore, PDO::PARAM_STR);
-            $s->bindValue(':locazione', $locazione, PDO::PARAM_STR);
-            $s->bindValue(':nome', $casaeditrice, PDO::PARAM_STR);
-            $s->bindValue(':anno_acquisto', $anno_acquisto, PDO::PARAM_INT);
-
-            $s->execute();
         }
-        catch (PDOException $e)
+        else
         {
-            $error = 'Errore nella ricerca.';
+            $titolo = "%%";
+
+        }
+        echo "Titolo: ".$_POST['institolo'];
+        if(isset($_POST['insautore']))
+        {
+            $autore = "%".$_POST['insautore']."%";
+        }
+        else
+        {
+            $autore = "%%";
+
+        }
+        echo "<br>Autore: ".$autore;
+        if(isset($_POST['inscasaeditrice']))
+        {
+            $casaeditrice = "%".trim($_POST['inscasaeditrice'])."%";
+
+        }
+        else
+        {
+            $casaeditrice = "%%";
+        }
+        echo "<br>CasaEditrice: ".$casaeditrice;
+
+        if(isset($_POST['inslocazione']))
+        {
+            $locazione = "%".trim($_POST['inslocazione'])."%";
+        }
+        else
+        {
+            $locazione = "%%";
+
+        }
+        echo "<br>Locazione: ".$locazione;
+
+        if(isset($_POST['insisbn']))
+        {
+            $isbn = "%".trim($_POST['insisbn'])."%";
+        }
+        else
+        {
+            $isbn = "%%";
+
+        }
+        echo "<br>ISBN: ".$isbn;
+
+        if(isset($_POST['insannoacquisto']))
+        {
+            $anno_acquisto = "%".trim($_POST['insannoacquisto'])."%";            }
+        else
+        {
+            $anno_acquisto = "%%";
+
+        }
+        echo "<br>annoacquisto: ".$anno_acquisto;
+        if($titolo == "%%" && $autore == "%%" && $locazione == "%%" && $casaeditrice == "%%" && $isbn == "%%" && $anno_acquisto == "%%")
+        {
+
+
+            //header("Location: admin.php");
+
             echo "<script language=\"JavaScript\">\n";
-            echo "alert(\"$error\");\n";
-            echo "</script>";
-            exit();
-        }
-
-        $risultati = $s->fetchAll();
-        if(isset($risultati))
-        {
-            $GLOBALS['risultati'] = $risultati;
-        }
-        else{
-            $error = 'La ricerca non ha prodotto risultati';
-            echo "<script language=\"JavaScript\">\n";
-            echo "alert(\"$error\");\n";
+            echo "alert(\"Per favore inserire i parametri per la ricerca\");\n";
             echo "</script>";
         }
-        //include 'daordinare.html.php';
-        echo "<script language=\"JavaScript\">\n";
-        echo "alert(\"Ricerca andata a buon fine\");\n";
-        echo "</script>";
+        else
+        {
+            try
+            {
+                $sql = 'SELECT * FROM libro INNER JOIN casa_editrice on libro.id_casa_editrice = casa_editrice.id_casa_editrice
+                WHERE titolo LIKE :titolo AND autore LIKE :autore AND locazione LIKE :locazione
+                AND nome LIKE :casaeditrice AND isbn LIKE :isbn AND anno_acquisto LIKE :anno_acquisto';
 
-        //header("Location: ricerca.php");
+                $s = $pdo->prepare($sql);
+                $s->bindValue(':titolo', $titolo, PDO::PARAM_STR);
+                $s->bindValue(':autore', $autore, PDO::PARAM_STR);
+                $s->bindValue(':locazione', $locazione, PDO::PARAM_STR);
+                $s->bindValue(':nome', $casaeditrice, PDO::PARAM_STR);
+                $s->bindValue(':isbn', $isbn, PDO::PARAM_INT);
+                $s->bindValue(':anno_acquisto', $anno_acquisto, PDO::PARAM_INT);
+
+                $s->execute();
+            }
+
+            catch (PDOException $e)
+            {
+                $error = 'Errore nella ricerca.';
+                echo "<script language=\"JavaScript\">\n";
+                echo "alert(\"$error\");\n";
+                echo "</script>";
+                exit();
+            }
+
+            $risultati = $s->fetchAll();
+
+            if(isset($risultati))
+            {
+                $GLOBALS['risultati'] = $risultati;
+            }
+            else{
+                $error = 'La ricerca non ha prodotto risultati';
+                echo "<script language=\"JavaScript\">\n";
+                echo "alert(\"$error\");\n";
+                echo "</script>";
+            }
+            //include 'daordinare.html.php';
+            echo "<script language=\"JavaScript\">\n";
+            echo "alert(\"Ricerca andata a buon fine\");\n";
+            echo "</script>";
+
+            //header("Location: ricerca.php");
+        }
     }
 ?>
