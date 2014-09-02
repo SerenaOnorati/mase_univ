5d858cf3050255e8b127978bc91009643ef0359a
Serena Onorati <serenaonorati@hotmail.it>
2014-09-02 13:18:48 +0200

    arrivato e ordina per distributore arrivato

--- ordina_libri.php
+++ ordina_libri.php
@@ -19,29 +19,53 @@ else{
         //inserimento della nuova news nel db
         try
         {
+
+
             /* Inizio la transazione */
             $pdo->beginTransaction();
 
-            $sql = 'INSERT INTO ordine (arrivato, ordinato)
-                    VALUES (:arrivato, :ordinato)';
-
-            $s = $pdo->prepare($sql);
-            $s->bindValue(':arrivato', false, PDO::PARAM_BOOL);
-            $s->bindValue(':ordinato', false, PDO::PARAM_BOOL);
-
-            $s->execute();
-
-            $id = $pdo->lastInsertId();
-
-            $sql = 'INSERT INTO ordine_libro (isbn, quantita_ordine, data_ordine, id_ordine)
-                    VALUES (:isbn, :quantita, :data_ordine, :id_ordine)';
+            $sql = 'SELECT COUNT(*) FROM ordine_libro WHERE isbn = :isbn';
             $s = $pdo->prepare($sql);
             $s->bindValue(':isbn', $isbn, PDO::PARAM_INT);
-            $s->bindValue(':quantita', $quantita, PDO::PARAM_INT);
-            $s->bindValue(':data_ordine', $data);
-            $s->bindValue(':id_ordine', $id, PDO::PARAM_INT);
-
             $s->execute();
+            $row = $s->fetch();
+            if($row[0] > 0)
+            {
+                $sql = 'UPDATE ordine_libro SET quantita_ordine = quantita_ordine + :quantita
+                        WHERE isbn = :isbn';
+
+                $s = $pdo->prepare($sql);
+                $s->bindValue(':isbn', $isbn, PDO::PARAM_INT);
+                $s->bindValue(':quantita', $quantita, PDO::PARAM_INT);
+                $s->execute();
+
+            }
+            else
+            {
+                $sql = 'INSERT INTO ordine (arrivato, ordinato)
+                    VALUES (:arrivato, :ordinato)';
+
+                $s = $pdo->prepare($sql);
+                $s->bindValue(':arrivato', false, PDO::PARAM_BOOL);
+                $s->bindValue(':ordinato', false, PDO::PARAM_BOOL);
+
+                $s->execute();
+
+                $id = $pdo->lastInsertId();
+
+                $sql = 'INSERT INTO ordine_libro (isbn, quantita_ordine, data_ordine, id_ordine)
+                    VALUES (:isbn, :quantita, :data_ordine, :id_ordine)';
+                $s = $pdo->prepare($sql);
+                $s->bindValue(':isbn', $isbn, PDO::PARAM_INT);
+                $s->bindValue(':quantita', $quantita, PDO::PARAM_INT);
+                $s->bindValue(':data_ordine', $data);
+                $s->bindValue(':id_ordine', $id, PDO::PARAM_INT);
+
+                $s->execute();
+            }
+
+
+
 
             /*Eseguo le operazioni della transazione*/
             $pdo->commit();
